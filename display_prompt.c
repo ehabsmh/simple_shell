42fad12eb5e69d61e6ab764b272c8977e2243c59
root <you@example.com>
2023-08-20 15:51:46 -0700

    display_prompt.c

---
+++ display_prompt.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+
+/**
+ * display_prompt - displays prompt to the terminal
+ * Return: void
+*/
+void display_prompt(void)
+{
+	char prompt[256];
+	char *p_prompt;
+
+	p_prompt = getcwd(prompt, sizeof(prompt));
+
+	if (!p_prompt)
+		printf("$ ");
+}
