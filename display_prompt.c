8e598a27c8e3018aaef9849f8f5116efbb15bfa4
Ehab Elsayed <ehabsmh3@gmail.com>
2023-08-24 04:53:24 +0300

    .

--- display_prompt.c
+++ display_prompt.c
@@ -6,23 +6,8 @@
  **/
 void display_prompt(void)
 {
-	char prompt[256];
-	char *p_prompt;
-	size_t p_prompt_len;
-
 	if (!isatty(STDIN_FILENO))
 		return;
 
-	p_prompt = getcwd(prompt, sizeof(prompt));
-
-	p_prompt_len = _strlen(p_prompt);
-
-	if (!p_prompt)
-		write(STDOUT_FILENO, "$ ", 2);
-	else
-	{
-		write(STDOUT_FILENO, p_prompt, p_prompt_len);
-		_putchar('$');
-		_putchar(' ');
-	}
+	write(STDOUT_FILENO, "#cisfun$ ", 9);
 }
