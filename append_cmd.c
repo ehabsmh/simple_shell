b684143d29f3352b182de4ee625079bc3a7e4e5c
Ehab Elsayed <ehabsmh3@gmail.com>
2023-08-24 04:08:00 +0300

    betty style added

--- append_cmd.c
+++ append_cmd.c
@@ -4,34 +4,34 @@
  * append_cmd - appends the cmd to the path
  * @cmd: the command-line
  * @path: The directory
- * 
+ *
  * Return: the appended command
 */
 char *append_cmd(char *cmd, char *path)
 {
-  char *appended_cmd;
-  size_t cmd_len, path_len, appended_cmd_len;
+	char *appended_cmd;
+	size_t cmd_len, path_len, appended_cmd_len;
 
-  if (!cmd || !path)
-    return (NULL);
+	if (!cmd || !path)
+		return (NULL);
 
-  cmd_len = _strlen(cmd);
-  path_len = _strlen(path);
+	cmd_len = _strlen(cmd);
+	path_len = _strlen(path);
 
-  appended_cmd = malloc(sizeof(char) * (cmd_len + path_len + 2));
-  if (!appended_cmd)
-    return (NULL);
+	appended_cmd = malloc(sizeof(char) * (cmd_len + path_len + 2));
+	if (!appended_cmd)
+		return (NULL);
 
-  _strcat(appended_cmd, path);
+	_strcat(appended_cmd, path);
 
-  if (appended_cmd && appended_cmd[path_len - 1] != '/')
-    _strcat(appended_cmd, "/");
+	if (appended_cmd && appended_cmd[path_len - 1] != '/')
+		_strcat(appended_cmd, "/");
 
-  _strcat(appended_cmd, cmd);
+	_strcat(appended_cmd, cmd);
 
-  appended_cmd_len = _strlen(appended_cmd); 
+	appended_cmd_len = _strlen(appended_cmd);
 
-  appended_cmd[appended_cmd_len] = '\0';
+	appended_cmd[appended_cmd_len] = '\0';
 
-  return (appended_cmd);
+	return (appended_cmd);
 }
